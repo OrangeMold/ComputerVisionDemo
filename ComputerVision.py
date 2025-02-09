5f94ee501d41f04506465b22adaa3e2d01368aae
OrangeMold <64652407+OrangeMold@users.noreply.github.com>
2025-02-09 09:07:24 -0500

    10 epoch model

--- ComputerVision.py
+++ ComputerVision.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import cv2
 
 # Load pre-trained model (you can use a custom-trained model if needed)
-model = YOLO("best.pt")  # Default small model, you can train a custom one
+model = YOLO("best10epoch.pt")  # Default small model, you can train a custom one
 
 # Open webcam
 cap = cv2.VideoCapture(0)
